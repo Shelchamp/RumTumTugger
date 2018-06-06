719b171b28a7853716eef3c186c58fb13d63f0a0
Sheldon <Sheld.Chan@gmail.com>
2018-06-05 19:11:37 -0700

    finish linking back end to front render

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,4 @@
+class StaticPagesController < ApplicationController
+  def static_page
+  end
+end
---
+++ frontend/rum_tum_entry.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+document.addEventListener("DOMContentLoaded", ()=>{
+
+  const root = document.getElementById('root');
+
+  ReactDOM.render(<h1>HHHHHHH</h1>, root);
+})
