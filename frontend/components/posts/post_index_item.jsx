15b2e1122fdd41746eadc0bbb25dd5fda2222ca5
Sheldon <Sheld.Chan@gmail.com>
2018-06-08 20:44:35 -0700

    style index items

--- frontend/components/posts/post_index_item.jsx
+++ frontend/components/posts/post_index_item.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const PostIndexItem = props => (
-  <div>
+  <div className='post-index-item'>
     <h2>{props.post.title}</h2>
     <h3>{props.post.body}</h3>
   </div>
