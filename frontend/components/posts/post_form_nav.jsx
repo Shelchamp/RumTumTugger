4b1b9f44ead41b481e51fec48aadca2dcc19b92c
Sheldon <Sheld.Chan@gmail.com>
2018-06-13 16:17:52 -0700

    center post nav and post form

--- frontend/components/posts/post_form_nav.jsx
+++ frontend/components/posts/post_form_nav.jsx
@@ -4,7 +4,7 @@ class PostFormNav extends React.Component {
 
   render(){
     return (
-      <li className='post-container new-post-buttons-container'>
+      <li className='new-post-buttons-container'>
         <div className='new-post-buttons post post-full is-mine with-avatar new-post'>
           <nav className='post-buttons clearfix'>
             <Link to='/feed/textform' className='new-post-label'>
