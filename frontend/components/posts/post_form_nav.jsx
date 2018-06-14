864bf08976de08209a6e44115b2df6e32f586172
Sheldon <Sheld.Chan@gmail.com>
2018-06-13 18:00:42 -0700

    refactor post form nav html

--- frontend/components/posts/post_form_nav.jsx
+++ frontend/components/posts/post_form_nav.jsx
@@ -7,21 +7,55 @@ class PostFormNav extends React.Component {
       <li className='new-post-buttons-container'>
         <div className='new-post-buttons post post-full is-mine with-avatar new-post'>
           <nav className='post-buttons clearfix'>
-            <Link to='/feed/textform' className='new-post-label'>
-              <i
-                className='icon-post-text'
-                ></i>
+            <Link to='/feed/textform' className='new-post-label' id='new-post-label-text'>
+              <i className='icon-post-text'></i>
               <span
                 className='new-post-label-text'>
                 Text
               </span>
             </Link>
-            <Link to='/' className='new-post-label'>Photo</Link>
-            <Link to='/' className='new-post-label'>Quote</Link>
-            <Link to='/' className='new-post-label'>Link</Link>
-            <Link to='/' className='new-post-label'>Chat</Link>
-            <Link to='/' className='new-post-label'>Audio</Link>
-            <Link to='/' className='new-post-label'>Video</Link>
+            <Link to='/' className='new-post-label' id='new-post-label-photo'>
+              <i className='icon-post-photo'></i>
+              <span
+                className='new-post-label-photo'>
+                Photo
+              </span>
+            </Link>
+            <Link to='/' className='new-post-label' id='new-post-label-quote'>
+              <i className='icon-post-quote'></i>
+              <span
+                className='new-post-label-quote'>
+                Quote
+              </span>
+            </Link>
+            <Link to='/' className='new-post-label' id='new-post-label-link'>
+              <i className='icon-post-link'></i>
+              <span
+                className='new-post-label-link'>
+                Link
+              </span>
+            </Link>
+            <Link to='/' className='new-post-label' id='new-post-label-chat'>
+              <i className='icon-post-chat'></i>
+              <span
+                className='new-post-label-chat'>
+                Chat
+              </span>
+            </Link>
+            <Link to='/' className='new-post-label' id='new-post-label-audio'>
+              <i className='icon-post-audio'></i>
+              <span
+                className='new-post-label-audio'>
+                Audio
+              </span>
+            </Link>
+            <Link to='/' className='new-post-label' id='new-post-label-video'>
+              <i className='icon-post-video'></i>
+              <span
+                className='new-post-label-video'>
+                Video
+              </span>
+            </Link>
           </nav>
           <div className='post-avatar tumblelog-avatar'></div>
         </div>
