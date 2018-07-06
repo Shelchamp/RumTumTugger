9f3decbab61e867bf40d5e42bbc17e1457def511
Sheldon <Sheld.Chan@gmail.com>
2018-07-06 12:06:31 -0700

    add live link to readme

--- README.md
+++ README.md
@@ -1,16 +1,19 @@
 # README
 
+[Rum Tum Tugger Live](https://rumtumtugger.herokuapp.com/#/login)
+
 ## About
+
 Rum Tum Tugger is a full stack application modeled on Tumblr, preserving its layout and cozy ambiance.
 While there is no rigid theme for Rum Tum Tugger, the goal of this application is to be a thought journal.
 Everyone is invited to sign up anonymously, post art, leave confessions, or simply peruse through Tugger's entries.
 Share a painting you've been working on for months, a thought that has been weighing on your mind, or a funny cat picture. Things can be as contemplative or silly you want, as long as you are having fun.
 
-
 ## Features
-* Creating a post
-* User Authenticaion
-* Project direction
+
+- Creating a post
+- User Authenticaion
+- Project direction
 
 ### Creating a post
 
@@ -20,7 +23,7 @@ Choose a post form from the nav bar. Currently supported post types are text, ph
 
 ## Project Direction
 
-* Reblogs and shares
-* Liking posts
-* Taggings posts and searching posts by tag
-* Following users
+- Reblogs and shares
+- Liking posts
+- Taggings posts and searching posts by tag
+- Following users
--- frontend/components/posts/post_index.jsx
+++ frontend/components/posts/post_index.jsx
@@ -1,38 +1,35 @@
-import React from 'react';
-import PostIndexItem from './post_index_item';
-import PostFormNav from './post_form_nav';
-import CreatePostContainer from './create_post_container';
-import {Route, Switch, Redirect} from 'react-router-dom';
+import React from "react";
+import PostIndexItem from "./post_index_item";
+import PostFormNav from "./post_form_nav";
+import CreatePostContainer from "./create_post_container";
+import { Route, Switch, Redirect } from "react-router-dom";
 class PostIndex extends React.Component {
-
-  componentDidMount(){
+  componentDidMount() {
     this.props.fetchPosts();
     this.props.fetchUsers();
     // debugger
   }
 
-
-  render(){
+  render() {
     // debugger
     return (
       <div className="post-index-container fade-in">
         <ol>
-            {/*
+          {/*
               */}
-          {
-            this.props.posts.map( post => (
+          {this.props.posts &&
+            this.props.users &&
+            this.props.posts.map(post => (
               <PostIndexItem
                 post={post}
                 key={post.id}
                 author={this.props.users[post.user_id]}
-
               />
-            ))
-          }
+            ))}
         </ol>
       </div>
-    )
+    );
   }
 }
 
-export default PostIndex
+export default PostIndex;
