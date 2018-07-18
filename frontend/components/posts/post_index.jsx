9fa28f98c109b1e9c0986732a5918ff3badff9f8
Sheldon <Sheld.Chan@gmail.com>
2018-07-17 17:26:31 -0700

    remove unnecessary file imports

--- frontend/components/posts/post_index.jsx
+++ frontend/components/posts/post_index.jsx
@@ -1,8 +1,9 @@
 import React from "react";
 import PostIndexItem from "./post_index_item";
-import PostFormNav from "./post_form_nav";
-import CreatePostContainer from "./create_post_container";
-import { Route, Switch, Redirect } from "react-router-dom";
+// import PostFormNav from "./post_form_nav";
+// import CreatePostContainer from "./create_post_container";
+// import { Route, Switch, Redirect } from "react-router-dom";
+
 class PostIndex extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
