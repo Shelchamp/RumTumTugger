81ea770a7f178b5262377bd8323f9131530eec7a
Sheldon <Sheld.chan@gmail.com>
2019-05-04 21:03:36 -0700

    Create Delete Like Actions.

--- frontend/actions/like_actions.js
+++ frontend/actions/like_actions.js
@@ -1,16 +1,22 @@
 import * as LikeApiUtil from "../util/like_api_util";
 export const RECEIVE_LIKE = "RECEIVE_LIKE";
 export const RECEIVE_ALL_LIKES = "RECEIVE_ALL_LIKES";
+export const DELETE_LIKE = "DELETE_LIKE";
 
 export const createLike = post => dispatch => (
   LikeApiUtil.createLike(post)
     .then(like => dispatch(receiveLike(like)))
 );
 
-export const fetchLikes = () => dispatch => {
+export const fetchLikes = () => dispatch => (
   LikeApiUtil.getLikes()
     .then(likes => dispatch(receiveAllLikes(likes)))
-};
+);
+
+export const deleteLike = post => dispatch => (
+  LikeApiUtil.deleteLike(post)
+    .then(like => dispatch(deleteLike(like)))
+)
 
 const receiveLike = like => ({
   type: RECEIVE_LIKE,
@@ -21,5 +27,10 @@ const receiveAllLikes = likes => ({
   type: RECEIVE_ALL_LIKES,
   likes
 });
+
+const deleteLike = like => ({
+  type: DELETE_LIKE,
+  like
+})
 
 export default receiveLike;
